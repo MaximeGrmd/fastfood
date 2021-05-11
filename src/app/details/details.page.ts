042e4b40cfa95c309e328fae6c7ed6f5c34a0f2d
MaximeGrmd <maxime.grimaud@insa-rennes.fr>
2021-05-11 11:34:00 +0200

    [Details] Add category slides.

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,22 +1,82 @@
+import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { IonContent, IonList, IonSlides } from '@ionic/angular';
+import { isPlatform } from '@ionic/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage implements OnInit, AfterViewInit {
+
+  @ViewChild(IonContent) content: IonContent;
+  @ViewChild(IonSlides) slides: IonSlides;
+  @ViewChildren(IonList, { read: ElementRef }) lists: QueryList<ElementRef>;
 
   data = null;
 
-  constructor(private http: HttpClient) { }
+  opts = {
+    freeMode: true,
+    slidesPerView: 2.6,
+    slidesOffsetBefore: 10,
+    slidesOffsetAfter: 150
+  };
+
+  categorySlidesVisible = false;
+
+  activeCategory = 0;
+  listElements = [];
+
+  constructor(
+    private http: HttpClient,
+    @Inject(DOCUMENT) private document: Document
+    ) { }
 
   ngOnInit() {
     this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/1.json').subscribe((res: any) => {
       this.data = res;
-      console.log(res);
     });
+
+    const headerHeight = isPlatform('ios') ? 44 : 56;
+
+    this.document.documentElement.style.setProperty('--header-position', `calc(env(safe-area-inset-top) + ${headerHeight}px)`);
+  }
+
+  ngAfterViewInit(): void {
+    this.lists.changes.subscribe( _ => {
+      this.listElements = this.lists.toArray();
+    });
+  }
+
+  selectCategory(index: number): void {
+    const child = this.listElements[index].nativeElement;
+    this.content.scrollToPoint(0, child.offsetTop - 120, 1000);
+
+  }
+
+  onScroll(event): void {
+    const offset = event.detail.scrollTop;
+    this.categorySlidesVisible = offset > 500;
+
+    for(let i = 0; i < this.listElements.length; i++) {
+      const item = this.listElements[i].nativeElement;
+      if(this.isElementInViewport(item)) {
+        this.activeCategory = i;
+        this.slides.slideTo(i, 1000);
+        break;
+      }
+    }
+  }
+
+  isElementInViewport(element): boolean {
+    const rect = element.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || this.document.documentElement.clientHeight)
+    );
   }
 
 }
